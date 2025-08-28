181f7f31c80dc4fcd91967afc189e87f1246d1b1
Alex Manos <manosalex22@gmail.com>
2025-08-28 15:43:08 -0400

    feat: connect frontend and backend api

--- apps/frontend/App.tsx
+++ apps/frontend/App.tsx
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { checkAPI } from './api';
 
 export default function App() {
-  return ( 
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [status, setStatus] = useState("Not connected");
+
+    useEffect(() => {
+        checkAPI()
+            .then((json) => {
+                if (json.ok) setStatus("Connected to API");
+            })
+            .catch((err) => setStatus(`Error: ${err.message}`))
+    }, []);
+
+    return ( 
+        <View style={styles.container}>
+            <Text>{status}</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ apps/frontend/api.ts
@@ -0,0 +1,11 @@
+import Constants from 'expo-constants';
+
+const baseURL: string = 
+        (Constants.expoConfig?.extra?.API_BASEURL as string) ||
+        "http://172.27.30.61:4000";
+
+export async function checkAPI() {
+    const res = await fetch(`${baseURL}/`);
+    if (!res.ok) throw new Error("Could not connect to API");
+    return res.json() as Promise<{ ok: boolean}>;
+}
