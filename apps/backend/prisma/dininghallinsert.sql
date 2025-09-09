eda5290d75910e3ec535c5ff7c49ba0c401e5385
Alex Manos <manosalex22@gmail.com>
2025-09-09 14:27:41 -0400

    chore(db): create script to insert/replace dininghall table

---
+++ apps/backend/prisma/dininghallinsert.sql
@@ -0,0 +1,31 @@
+INSERT or REPLACE INTO DiningHall
+    (id, name, breakfaststart, breakfastend, lunchstart, lunchend, dinnerstart, dinnerend, haslatenight, hasgrabngo)
+    VALUES (5, 'McMahon', '07:00', '10:45', '11:00', '15:00', '16:30', '19:15', false, false);
+
+INSERT or REPLACE INTO DiningHall
+    (id, name, breakfaststart, breakfastend, lunchstart, lunchend, dinnerstart, dinnerend, haslatenight, hasgrabngo)
+    VALUES (1, 'Whitney', '07:00', '10:45', '11:00', '15:00', '16:30', '19:15', false, false);
+
+INSERT or REPLACE INTO DiningHall
+    (id, name, breakfaststart, breakfastend, lunchstart, lunchend, dinnerstart, dinnerend, haslatenight, hasgrabngo)
+    VALUES (7, 'North', '07:00', '10:45', '11:00', '15:00', '16:30', '19:15', false, false);
+
+INSERT or REPLACE INTO DiningHall
+    (id, name, breakfaststart, breakfastend, lunchstart, lunchend, dinnerstart, dinnerend, haslatenight, hasgrabngo)
+    VALUES (16, 'South', '07:00', '10:45', '11:00', '15:00', '16:30', '19:15', true, false);
+
+INSERT or REPLACE INTO DiningHall
+    (id, name, breakfaststart, breakfastend, lunchstart, lunchend, dinnerstart, dinnerend, haslatenight, hasgrabngo)
+    VALUES (42, 'Towers', '07:00', '10:45', '11:00', '15:00', '16:30', '19:15', false, true);
+
+INSERT or REPLACE INTO DiningHall
+    (id, name, breakfaststart, breakfastend, lunchstart, lunchend, dinnerstart, dinnerend, haslatenight, hasgrabngo)
+    VALUES (3, 'Connecticut', '07:00', '10:45', '11:00', '14:30', '16:00', '19:15', false, false);
+
+INSERT or REPLACE INTO DiningHall
+    (id, name, breakfaststart, breakfastend, lunchstart, lunchend, dinnerstart, dinnerend, haslatenight, hasgrabngo)
+    VALUES (6, 'Putnam', '07:00', '10:45', '11:00', '14:30', '16:00', '19:15', false, true);
+
+INSERT or REPLACE INTO DiningHall
+    (id, name, breakfaststart, breakfastend, lunchstart, lunchend, dinnerstart, dinnerend, haslatenight, hasgrabngo)
+    VALUES (15, 'Northwest', '07:00', '10:45', '11:00', '14:15', '15:45', '19:15', true, false);
